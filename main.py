589dfaf29d5c55b77842ee5482f2d866b9b094f8
CoopStaz <cjstaz13@gmail.com>
2024-08-13 18:01:00 +1000

    Add basic label and button for macro creation

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ tk = Tk()
 tk.title("Macro Bank")
 tk.configure(background="#bdeaff")
 tk.minsize(600, 800)
+tk.columnconfigure(0, weight=1)
+tk.columnconfigure(1, weight=1)
+tk.columnconfigure(2, weight=1)
+
+record_title = Label(tk, text="Macro Recording")
+record_title.grid(row=0, column=1, pady=10)
+
+record_btn = Button(tk, text="Record", bg='red')
+record_btn.grid(row=1, column=1)
 
 
 tk.mainloop()
