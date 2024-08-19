303e888f7069b00a0082baa9f5e49618f6a32384
CoopStaz <cjstaz13@gmail.com>
2024-08-19 16:46:17 +1000

    Assign all images as a varible

--- main.py
+++ main.py
@@ -19,6 +19,21 @@ record_btn = Button(tk, text="Record", bg='red', command=recorder.record)
 record_title.grid(row=0, column=1, pady=10)
 record_btn.grid(row=1, column=1)
 
+# Define all map images to PhotoImages
+abyss_img = PhotoImage(file='images/valorant_maps/abyss.png')
+ascent_img = PhotoImage(file='images/valorant_maps/ascent.png')
+bind_img = PhotoImage(file='images/valorant_maps/bind.png')
+breeze_img = PhotoImage(file='images/valorant_maps/breeze.png')
+fracture_img = PhotoImage(file='images/valorant_maps/fracture.png')
+haven_img = PhotoImage(file='images/valorant_maps/haven.png')
+icebox_img = PhotoImage(file='images/valorant_maps/icebox.png')
+lotus_img = PhotoImage(file='images/valorant_maps/lotus.png')
+pearl_img = PhotoImage(file='images/valorant_maps/pearl.png')
+split_img = PhotoImage(file='images/valorant_maps/split.png')
+sunset_img = PhotoImage(file='images/valorant_maps/sunset.png')
+
+
+
 
 
 tk.mainloop()
