cc5acbb7228b7b5300000a474c9cbe888fef7572
CoopStaz <cjstaz13@gmail.com>
2024-08-19 17:16:03 +1000

    Creat function to load an resize the button images

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from PIL import Image, ImageTk
 from record_macro import Recorder
 
 # Assign basic UI layout
@@ -16,24 +17,17 @@ recorder = Recorder()
 record_title = Label(tk, text="Macro Recording")
 record_btn = Button(tk, text="Record", bg='red', command=recorder.record)
 
-record_title.grid(row=0, column=1, pady=10)
-record_btn.grid(row=1, column=1)
+record_title.grid(row=0, column=0, pady=10)
+record_btn.grid(row=1, column=0)
 
-# Define all map images to PhotoImages
-abyss_img = PhotoImage(file='images/valorant_maps/abyss.png')
-ascent_img = PhotoImage(file='images/valorant_maps/ascent.png')
-bind_img = PhotoImage(file='images/valorant_maps/bind.png')
-breeze_img = PhotoImage(file='images/valorant_maps/breeze.png')
-fracture_img = PhotoImage(file='images/valorant_maps/fracture.png')
-haven_img = PhotoImage(file='images/valorant_maps/haven.png')
-icebox_img = PhotoImage(file='images/valorant_maps/icebox.png')
-lotus_img = PhotoImage(file='images/valorant_maps/lotus.png')
-pearl_img = PhotoImage(file='images/valorant_maps/pearl.png')
-split_img = PhotoImage(file='images/valorant_maps/split.png')
-sunset_img = PhotoImage(file='images/valorant_maps/sunset.png')
+map_names = ['abyss', 'ascent', 'bind', 'breeze', 'fracture', 'haven', 'icebox', 'lotus', 'pearl', 'split', 'sunset']
 
 
-
+# Define a function to load and resize images
+def load_and_resize_image(filename, size=(300, 300)):
+    img = Image.open(f"images/valorant_maps/{filename}")
+    img = img.resize(size, Image.ANTIALIAS)
+    return ImageTk.PhotoImage(img)
 
 
 tk.mainloop()
